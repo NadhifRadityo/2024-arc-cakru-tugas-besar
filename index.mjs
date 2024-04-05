0aa3ef46c0e3e8e91ee7814b9e8fe5fe25399076
nazwapranoto <129711996+nazwapranoto@users.noreply.github.com>
2024-04-05 13:49:58 +0700

    Setup dependencies and endpoints

--- index.mjs
+++ index.mjs
@@ -1,5 +1,15 @@
+import * as url from "url";
+import path from "path";
+import fs0 from "fs";
+import fs from "fs/promises";
 import express from "express";
+import expressWs from "express-ws";
+import compression from "express-compression";
+import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
+import { spawn } from "child_process";
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const { PORT } = Object.assign({ PORT: 3000 }, process.env);
 
 (async () => {
@@ -12,6 +22,22 @@ const { PORT } = Object.assign({ PORT: 3000 }, process.env);
 			res.socket.setNoDelay(true);
 		} finally { return next(); }
 	});
+	app.use(compression({
+		brotli: { enabled: true },
+		filter: (req, res) => {
+			const type = res.getHeader('Content-Type')?.trim();
+			if(type == null) return false;
+			if(compression.filter(req, res)) return true;
+			return [
+				"image/webp",
+				"video/mp4",
+				"video/mp2t",
+				"application/vnd.apple.mpegurl"
+			]
+				.some(t => type.includes(t));
+		}
+	}));
+	app.use(express.static(path.join(__dirname, "public/")));
 
 	app.get("/", (req, res) => {
 		res.render("main");
