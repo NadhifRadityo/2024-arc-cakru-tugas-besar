769da328f1c41c7c67a6002a3b6619fdc01ed6fa
Nadhif Radityo <nadhifradityo@gmail.com>
2024-04-05 13:35:33 +0700

    feat(project): Added initial pages

--- index.mjs
+++ index.mjs
@@ -4,6 +4,23 @@ const { PORT } = Object.assign({ PORT: 3000 }, process.env);
 
 (async () => {
 	const app = express();
+
+	app.set("view engine", "ejs");
+	app.use((req, res, next) => {
+		try {
+			req.socket.setNoDelay(true);
+			res.socket.setNoDelay(true);
+		} finally { return next(); }
+	});
+
+	app.get("/", (req, res) => {
+		res.render("main");
+	});
+	app.get("/viewer/:link", (req, res) => {
+		const { link } = req.params;
+		res.render("viewer", { link });
+	});
+
 	app.listen(PORT, () => {
 		console.log(`Server started on port ${PORT}`);
 	});
